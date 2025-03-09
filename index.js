82d7684abb526c71f18ea22aec60248cdb703994
PsourceC <pollock.scottj@gmail.com>
2025-03-09 07:48:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const express = require('express');\nconst app = express();\nconst PORT = process.env.PORT || 3000;\n\napp.use(express.static(__dirname));\n\napp.get('/', (req, res) => {\n  res.sendFile(__dirname + '/index.html');\n});\n\napp.listen(PORT, '0.0.0.0', () => {\n  console.log();\n});
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// Respond with index.html for GET requests to the root URL
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Start server
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
